67f893a990da07d3ad1f9468bfde4cb13b312bfa
 Галина Муравьева <pazl992@mail.ru>
2019-03-06 15:04:15 +0300

    add framework

---
+++ PyrusServiceDeskIOS/PyrusServiceDesk.framework/Headers/PyrusServiceDesk.h
@@ -0,0 +1,17 @@
+//
+//  PyrusServiceDesk.h
+//  PyrusServiceDesk
+//
+//  Created by  Галина Муравьева on 23.01.2019.
+//  Copyright © 2019  Галина Муравьева. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//! Project version number for PyrusServiceDesk.
+FOUNDATION_EXPORT double PyrusServiceDeskVersionNumber;
+
+//! Project version string for PyrusServiceDesk.
+FOUNDATION_EXPORT const unsigned char PyrusServiceDeskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PyrusServiceDesk/PublicHeader.h>
+
