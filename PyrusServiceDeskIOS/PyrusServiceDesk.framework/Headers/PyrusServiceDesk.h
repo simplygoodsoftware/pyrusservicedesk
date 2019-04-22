1d755844e48712b77a746300f4d7ab25ca170f29
 Галина Муравьева <pazl992@mail.ru>
2019-04-22 17:06:48 +0300

    1.0.20 update for swift 5

--- PyrusServiceDeskIOS/PyrusServiceDesk.framework/Headers/PyrusServiceDesk.h
+++ PyrusServiceDeskIOS/PyrusServiceDesk.framework/Headers/PyrusServiceDesk.h
@@ -6,5 +6,3 @@ FOUNDATION_EXPORT double PyrusServiceDeskVersionNumber;
 //! Project version string for PyrusServiceDesk.
 FOUNDATION_EXPORT const unsigned char PyrusServiceDeskVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <PyrusServiceDesk/PublicHeader.h>
-
