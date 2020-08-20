9a89d0b61a20dd0fc708fd4adaf784d3e2972eb8
Kirill Lybov <libovko@mail.ru>
2020-08-20 14:41:10 +0300

    changes

---
+++ PyrusServiceDeskIOS/Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Kirill Lybov on 20.08.2020.
+//  Copyright © 2020  Галина Муравьева. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
