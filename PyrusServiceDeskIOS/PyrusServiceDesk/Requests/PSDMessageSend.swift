91c8ce6b1a49ada74b45816135d0a70bb63dc45c
Kirill Lybov <k.lybov@articul.ru>
2020-09-28 15:15:41 +0300

    fix

--- PyrusServiceDeskIOS/PyrusServiceDesk/Requests/PSDMessageSend.swift
+++ PyrusServiceDeskIOS/PyrusServiceDesk/Requests/PSDMessageSend.swift
@@ -78,7 +78,7 @@ struct PSDMessageSend {
     ///If first message sending end with error - error will be passed to all messages in array.
     static private var passQueueArray = [PSDMessage]()
     
-    static private let dispatchQueue = DispatchQueue(label: "PSDSendMesasges")
+    static private let dispatchQueue = DispatchQueue(label: "PSDSendMesasges", attributes: .concurrent)
     /**
      Pass message to server.
      - parameter messageToPass: PSDMessage need to be passed.
