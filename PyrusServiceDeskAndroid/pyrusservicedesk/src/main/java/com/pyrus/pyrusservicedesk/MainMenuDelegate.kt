5bf35feefbd93c81d1532e7c6a6eb2fd6c095e2f
BoyarinMax <mboyarkin@pyrus.com>
2020-10-08 11:31:47 +0300

    fix merge

--- PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/MainMenuDelegate.kt
+++
@@ -1,7 +0,0 @@
-
-interface MainMenuDelegate {
-
-    fun onCreateOptionsMenu(menu: Menu, activity: Activity): Boolean
-
-    fun onOptionsItemSelected(item: MenuItem, activity: Activity): Boolean
-}
