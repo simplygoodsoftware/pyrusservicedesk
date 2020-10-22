c355c4107959b2c5d980f4f10bc0acf44719deb8
VitalyKuznetsov <vkuznetsov@pyrus.com>
2020-10-22 08:29:29 +0300

    Added keep for not obfuscating intermediate data models

--- PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/sdk/data/intermediate/Comments.kt
+++ PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/sdk/data/intermediate/Comments.kt
@@ -1,11 +1,13 @@
 package com.pyrus.pyrusservicedesk.sdk.data.intermediate
 
+import androidx.annotation.Keep
 import com.google.gson.annotations.SerializedName
 import com.pyrus.pyrusservicedesk.sdk.data.Comment
 
 /**
  * Intermediate data for parsing list of comments object
  */
+@Keep
 internal data class Comments(
     @SerializedName("comments")
     val comments: List<Comment> = emptyList(),
