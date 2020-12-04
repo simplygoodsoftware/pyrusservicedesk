d137510523b77f45415066e5ecf20b447cb6b82d
Lina <pazl992@mail.ru>
2020-12-04 14:33:13 +0300

    //

--- PyrusServiceDeskIOS/PyrusServiceDesk/Chat/messageInput/PSDRateView.swift
+++ PyrusServiceDeskIOS/PyrusServiceDesk/Chat/messageInput/PSDRateView.swift
@@ -56,4 +56,22 @@ class PSDRateView: UIView {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if #available(iOS 13.0, *) {
+            guard self.traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) else {
+                return
+            }
+            recolor()
+        }
+    }
+    private func recolor() {
+        for view in stackView.arrangedSubviews {
+            guard let button = view as? UIButton else {
+                continue
+            }
+            button.setBackgroundColor(color: PSD_lightGrayViewColor, forState: .normal)
+            button.setBackgroundColor(color: PSD_grayViewColor, forState: .highlighted)
+        }
+    }
 }
--- PyrusServiceDeskIOS/PyrusServiceDesk/Navigation/PSDViewController.swift
+++ PyrusServiceDeskIOS/PyrusServiceDesk/Navigation/PSDViewController.swift
@@ -2,21 +2,37 @@ import Foundation
 class PSDViewController: UIViewController {
     override var title: String? {
         didSet {
-            guard PyrusServiceDesk.mainController?.customization?.chatTitleColor != nil || PyrusServiceDesk.mainController?.customization?.customFontName != nil  else {
-                return
-            }
-            navigationItem.titleView = customNavigationTitle()
-            view.sizeToFit()
-            navigationController?.navigationBar.layoutIfNeeded()
+            designNavigation()
         }
     }
     private func customNavigationTitle() -> UIView? {
         let label = UILabel()
-        label.textColor = PyrusServiceDesk.mainController?.customization?.chatTitleColor ?? .psdLabel
+        var textColor = PyrusServiceDesk.mainController?.customization?.chatTitleColor ??  .psdLabel
+        if let color = PyrusServiceDesk.mainController?.customization?.customBarColor {
+            textColor = UIColor.getTextColor(for: color)
+        }
+        label.textColor = textColor
         label.font = .titleFont
         label.text = title
         label.lineBreakMode = .byTruncatingTail
         return label
+    }
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        super.traitCollectionDidChange(previousTraitCollection)
+        if #available(iOS 13.0, *) {
+            guard self.traitCollection.hasDifferentColorAppearance(comparedTo: previousTraitCollection) else {
+                return
+            }
+            designNavigation()
+        }
+    }
+    private func designNavigation() {
+        guard PyrusServiceDesk.mainController?.customization?.chatTitleColor != nil || PyrusServiceDesk.mainController?.customization?.customFontName != nil  else {
+            return
+        }
+        navigationItem.titleView = customNavigationTitle()
+        view.sizeToFit()
+        navigationController?.navigationBar.layoutIfNeeded()
     }
 }
 private extension UIFont {
