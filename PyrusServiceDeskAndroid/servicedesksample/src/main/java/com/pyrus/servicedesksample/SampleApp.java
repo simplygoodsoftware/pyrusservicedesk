64b087300a7336045eda8d8464e76e86efabd521
Boyarkin Maxim <mboyarkin@pyrus.com>
2021-06-10 11:54:31 +0300

    wip

--- PyrusServiceDeskAndroid/servicedesksample/src/main/java/com/pyrus/servicedesksample/SampleApp.java
+++ PyrusServiceDeskAndroid/servicedesksample/src/main/java/com/pyrus/servicedesksample/SampleApp.java
@@ -7,16 +7,14 @@ import com.pyrus.pyrusservicedesk.PyrusServiceDesk;
 
 public class SampleApp extends Application {
 
-    public static final String APP_ID = "Hsdzu8HtZfQtQDv6avpE~Fh8asQPZ32ScwI4k0MSTaJg7W~TcPydLXQMSvQ-dQas30AwKWqBnfkyUzV9fbNSvdCTgRvEc8lLG8JjAbzT66Yc9DfjHaw7LXUfIqJ9ezSIYASlgw==";
-
     @Override
     public void onCreate() {
         super.onCreate();
         PyrusServiceDesk.init(
                 this,
-                APP_ID,
-                "394513",
-                "vVIE+SQdDreBsS9GIewTfT+fIxuCAoppuIfjmT5DbpA/t42sdTwetanQr9z6cmXujDZen44A5Pi+c/Zryv26/g==",
+                "my_app_id",
+                "user_id",
+                "security_key",
                 true
         );
 
