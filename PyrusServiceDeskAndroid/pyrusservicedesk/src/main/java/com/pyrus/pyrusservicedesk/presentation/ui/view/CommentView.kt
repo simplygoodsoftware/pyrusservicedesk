477d9ae6433d83031abda4cbe17f961649e53130
Max <smartfoxdeveloper@gmail.com>
2022-11-09 16:25:37 +0400

    1.5.11

--- PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/presentation/ui/view/CommentView.kt
+++ PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/presentation/ui/view/CommentView.kt
@@ -303,7 +303,6 @@ internal class CommentView @JvmOverloads constructor(
      * Works with [ContentType.Text].
      */
     fun setCommentText(text: String) {
-        comment_text.setLinkTextColor(ConfigUtils.getAccentColor(context))
         val filteredText = text.replace("<br>", "\n").replace(Regex("\\n?<button>(.*?)</button>|\\n *?$|^ *?\\n"), "")
         comment_text.text = replaceLinkTagsWithSpans(filteredText)
         LinkifyCompat.addLinks(comment_text, Linkify.WEB_URLS or Linkify.PHONE_NUMBERS)
