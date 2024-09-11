a738bef4c708ddefaee8c59d67d89fed38c76d40
Max <smartfoxdeveloper@gmail.com>
2024-09-11 11:00:40 +0300

    ....

--- PyrusServiceDeskAndroid/servicedesksample/src/main/java/com/pyrus/servicedesksample/SampleApp.java
+++ PyrusServiceDeskAndroid/servicedesksample/src/main/java/com/pyrus/servicedesksample/SampleApp.java
@@ -17,9 +17,9 @@ public class SampleApp extends Application {
 
         map.put("test_text", "test some text");
         map.put("test_number", "999");
-        map.put("test_money", "ddd");
-        map.put("test_phone", "fddfdf");
-        map.put("test_email", "sample@email");
+        map.put("test_money", "555");
+        map.put("test_phone", "79778888888");
+        map.put("test_email", "sample@email.com");
 
 //        dodo sample
 //        "GJv5lsZnNQM8jDwppzj8Ciuwm5MiuQsF41nXoUkNut2qETsjtJz0F271-~M6WFfukoW6YJeiMZAjKzAiehLIFkklDpQ1Spap0Yd9csgIdVJTAdCL0ZF~bPl6qLvH0UfnFY-Z8A=="
@@ -27,7 +27,7 @@ public class SampleApp extends Application {
             this,
             "0HUi7grFuWVFHqWtL3f5YD-4PYJXiOEoLfCDb2yhTthkHBpedNbNU4O01YD2OnsSpvbMiXmweUF8akomZZIW1Ilb-W9mOPuK70L4lCI1mK0dJqXYUp0l-MJlsUv9tr8dSmKCSw==",
             "dev.pyrus.com",
-            false,
+            true,
             null,
             map
         );
