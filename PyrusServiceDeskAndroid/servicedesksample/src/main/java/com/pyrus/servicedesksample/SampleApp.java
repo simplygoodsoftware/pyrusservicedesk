a4f3f00dc142d609c3a254ab9ab6812c36e17648
Max <smartfoxdeveloper@gmail.com>
2024-09-17 13:40:13 +0300

    add language fieldData

--- PyrusServiceDeskAndroid/servicedesksample/src/main/java/com/pyrus/servicedesksample/SampleApp.java
+++ PyrusServiceDeskAndroid/servicedesksample/src/main/java/com/pyrus/servicedesksample/SampleApp.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.pyrus.pyrusservicedesk.PyrusServiceDesk;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class SampleApp extends Application {
@@ -15,11 +16,8 @@ public class SampleApp extends Application {
 
         Map<String, String> map = new HashMap<>();
 
-        map.put("test_text", "test some text");
-        map.put("test_number", "999");
-        map.put("test_money", "555");
-        map.put("test_phone", "79778888888");
-        map.put("test_email", "sample@email.com");
+        String lang = Locale.getDefault().getLanguage();
+        map.put("language", lang);
         PyrusServiceDesk.setFieldsData(map);
 
 //        dodo sample
