9565eb25e1aa977dd503a6a4ad73197414629831
sbobruskina <sbobruskina@pyrus.com>
2024-11-18 11:50:22 +0300

    bug fix

--- PyrusServiceDeskIOS/PyrusServiceDesk/Managers/SyncManager.swift
+++ PyrusServiceDeskIOS/PyrusServiceDesk/Managers/SyncManager.swift
@@ -25,89 +25,92 @@ class SyncManager {
         shouldSendAnotherRequest = false
 
         PyrusServiceDesk.repository.load { result in
+            let ticketCommands: [TicketCommand]
             switch result {
             case .success(let commands):
-                PSDGetChats.get(commands: commands.map({ $0.toDictionary() })) { [weak self] chats, commandsResult, authorAccessDenied, clientsArray in
-                    guard let self = self else { return }
-                    var clients = clientsArray
-                    PyrusServiceDesk.accessDeniedIds = authorAccessDenied ?? []
-                    if let authorAccessDenied, authorAccessDenied.count > 0 {
-                        DispatchQueue.main.async {
-                            PyrusServiceDesk.deniedAccessCallback?.deleteUsers(userIds: authorAccessDenied)
-                        }
-                        NotificationCenter.default.post(name: SyncManager.updateAccessesNotification, object: nil)
-                        
-                        if let clientsArray {
-                            for client in clientsArray {
-                                var removeClient = true
-                                for user in PyrusServiceDesk.additionalUsers {
-                                    if user.clientId == client.clientId && !authorAccessDenied.contains(user.userId) {
-                                        removeClient = false
-                                        break
-                                    }
-                                }
-                                if client.clientId == PyrusServiceDesk.clientId && !authorAccessDenied.contains(PyrusServiceDesk.customUserId ?? PyrusServiceDesk.userId) {
-                                    continue
-                                }
-                                
-                                if !removeClient { continue }
-                                if PyrusServiceDesk.currentClientId == client.clientId {
-                                    PyrusServiceDesk.currentClientId = nil
-                                }
-                                clients?.removeAll(where: { $0.clientId == client.clientId })
-                            }
-                        }
-                    }
-                    if let clients, clients.count > 0 {
-                        PyrusServiceDesk.clients = clients
-                    }
-                    
-                    if self.isFilter {
-                        NotificationCenter.default.post(name: PyrusServiceDesk.usersUpdateNotification, object: nil)
-                        self.isFilter = false
-                    }
-                    
-                    if let commandsResult {
-                        self.commandsResult = commandsResult
-                        NotificationCenter.default.post(name: SyncManager.commandsResultNotification, object: nil)
-                        for commandResult in commandsResult {
-                            PyrusServiceDesk.repository.deleteCommand(withId: commandResult.commandId)
-                            if let message = self.sendingMessages.first(where: { $0.commandId.lowercased() == commandResult.commandId.lowercased() })?.message {
-                                PSDMessagesStorage.removeFromStorage(messageId: message.clientId)
-                                if commandResult.error != nil {
-                                    message.state = .cantSend
-                                    PSDMessagesStorage.saveInStorage(message: message)
-                                }
-                            }
-                        }
-                    }
-                                
+                ticketCommands = commands
+            case .failure(_):
+                ticketCommands = []
+            }
+            
+            PSDGetChats.get(commands: ticketCommands.map({ $0.toDictionary() })) { [weak self] chats, commandsResult, authorAccessDenied, clientsArray in
+                guard let self = self else { return }
+                var clients = clientsArray
+                PyrusServiceDesk.accessDeniedIds = authorAccessDenied ?? []
+                if let authorAccessDenied, authorAccessDenied.count > 0 {
                     DispatchQueue.main.async {
-                        PyrusLogger.shared.logEvent("PSDGetChats did end with chats count: \(chats?.count ?? 0).")
-                        guard let chats = chats else{
-                            return
-                        }
-                        var unreadChats = 0
-                        var lasMessage: PSDMessage?
-                        for chat in chats {
-                            lasMessage = chat.messages.last
-                            guard !chat.isRead else{
+                        PyrusServiceDesk.deniedAccessCallback?.deleteUsers(userIds: authorAccessDenied)
+                    }
+                    NotificationCenter.default.post(name: SyncManager.updateAccessesNotification, object: nil)
+                    
+                    if let clientsArray {
+                        for client in clientsArray {
+                            var removeClient = true
+                            for user in PyrusServiceDesk.additionalUsers {
+                                if user.clientId == client.clientId && !authorAccessDenied.contains(user.userId) {
+                                    removeClient = false
+                                    break
+                                }
+                            }
+                            if client.clientId == PyrusServiceDesk.clientId && !authorAccessDenied.contains(PyrusServiceDesk.customUserId ?? PyrusServiceDesk.userId) {
                                 continue
                             }
-                            unreadChats = unreadChats + 1
+                            
+                            if !removeClient { continue }
+                            if PyrusServiceDesk.currentClientId == client.clientId {
+                                PyrusServiceDesk.currentClientId = nil
+                            }
+                            clients?.removeAll(where: { $0.clientId == client.clientId })
                         }
-                        
-                        UnreadMessageManager.refreshNewMessagesCount(unreadChats > 0, lastMessage: lasMessage)
-                    }
-
-                    self.isRequestInProgress = false
-                    
-                    if self.shouldSendAnotherRequest {
-                        self.syncGetTickets()
                     }
                 }
-            case .failure(_):
-                break
+                if let clients, clients.count > 0 {
+                    PyrusServiceDesk.clients = clients
+                }
+                
+                if self.isFilter {
+                    NotificationCenter.default.post(name: PyrusServiceDesk.usersUpdateNotification, object: nil)
+                    self.isFilter = false
+                }
+                
+                if let commandsResult {
+                    self.commandsResult = commandsResult
+                    NotificationCenter.default.post(name: SyncManager.commandsResultNotification, object: nil)
+                    for commandResult in commandsResult {
+                        PyrusServiceDesk.repository.deleteCommand(withId: commandResult.commandId)
+                        if let message = self.sendingMessages.first(where: { $0.commandId.lowercased() == commandResult.commandId.lowercased() })?.message {
+                            PSDMessagesStorage.removeFromStorage(messageId: message.clientId)
+                            if commandResult.error != nil {
+                                message.state = .cantSend
+                                PSDMessagesStorage.saveInStorage(message: message)
+                            }
+                        }
+                    }
+                }
+                            
+                DispatchQueue.main.async {
+                    PyrusLogger.shared.logEvent("PSDGetChats did end with chats count: \(chats?.count ?? 0).")
+                    guard let chats = chats else{
+                        return
+                    }
+                    var unreadChats = 0
+                    var lasMessage: PSDMessage?
+                    for chat in chats {
+                        lasMessage = chat.messages.last
+                        guard !chat.isRead else{
+                            continue
+                        }
+                        unreadChats = unreadChats + 1
+                    }
+                    
+                    UnreadMessageManager.refreshNewMessagesCount(unreadChats > 0, lastMessage: lasMessage)
+                }
+
+                self.isRequestInProgress = false
+                
+                if self.shouldSendAnotherRequest {
+                    self.syncGetTickets()
+                }
             }
         }
     }
