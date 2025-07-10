1bd406543bda669f93889fac30e0fb716cf6f190
epryamonosova <epryamonosova@pyrus.com>
2025-07-10 16:14:27 +0300

    clean adapter

--- PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/presentation/ui/navigation_page/ticket/TicketAdapter.kt
+++ PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/presentation/ui/navigation_page/ticket/TicketAdapter.kt
@@ -23,7 +23,6 @@ import com.pyrus.pyrusservicedesk.presentation.ui.navigation_page.ticket.entries
 import com.pyrus.pyrusservicedesk.presentation.ui.navigation_page.ticket.entries.ButtonsEntry
 import com.pyrus.pyrusservicedesk.presentation.ui.navigation_page.ticket.entries.CommentEntry
 import com.pyrus.pyrusservicedesk.presentation.ui.navigation_page.ticket.entries.DateEntry
-import com.pyrus.pyrusservicedesk.presentation.ui.navigation_page.ticket.entries.RatingEntry
 import com.pyrus.pyrusservicedesk.presentation.ui.navigation_page.ticket.entries.TicketEntry
 import com.pyrus.pyrusservicedesk.presentation.ui.navigation_page.ticket.entries.Type
 import com.pyrus.pyrusservicedesk.presentation.ui.navigation_page.ticket.entries.WelcomeMessageEntry
@@ -45,7 +44,6 @@ private const val VIEW_TYPE_COMMENT_INBOUND = 0
 private const val VIEW_TYPE_COMMENT_OUTBOUND = 1
 private const val VIEW_TYPE_WELCOME_MESSAGE = 2
 private const val VIEW_TYPE_DATE = 3
-private const val VIEW_TYPE_RATING = 4
 private const val VIEW_TYPE_COMMENT_RATING = 5
 private const val VIEW_TYPE_COMMENT_BUTTONS = 6
 
@@ -85,7 +83,6 @@ internal class TicketAdapter: AdapterBase<TicketEntry>() {
             return@with when {
                 type == Type.Date -> VIEW_TYPE_DATE
                 type == Type.WelcomeMessage -> VIEW_TYPE_WELCOME_MESSAGE
-                type == Type.Rating -> VIEW_TYPE_RATING
                 type == Type.Buttons -> VIEW_TYPE_COMMENT_BUTTONS
                 (this as CommentEntry).comment.rating != null -> VIEW_TYPE_COMMENT_RATING
                 this.comment.isInbound -> VIEW_TYPE_COMMENT_OUTBOUND
@@ -100,7 +97,6 @@ internal class TicketAdapter: AdapterBase<TicketEntry>() {
             VIEW_TYPE_COMMENT_INBOUND -> InboundCommentHolder(parent)
             VIEW_TYPE_COMMENT_OUTBOUND -> OutboundCommentHolder(parent)
             VIEW_TYPE_WELCOME_MESSAGE -> WelcomeMessageHolder(parent)
-            VIEW_TYPE_RATING -> RatingHolder(parent)
             VIEW_TYPE_COMMENT_RATING -> RatingCommentHolder(parent)
             VIEW_TYPE_COMMENT_BUTTONS -> ButtonsHolder(parent)
             else -> DateViewHolder(parent)
@@ -354,108 +350,6 @@ internal class TicketAdapter: AdapterBase<TicketEntry>() {
             date.text = item.date
         }
     }
-
-    private inner class RatingHolder(parent: ViewGroup) :
-        ViewHolderBase<RatingEntry>(parent, R.layout.psd_view_holder_rating) {
-
-//
-//        private var listEntry: List<SimpleTextRatingEntry> = emptyList()
-//        private val adapter = RatingAdapter(listEntry)
-//
-//        //private val ratingRv = itemView.findViewById<RecyclerView>(R.id.ratingRv)
-//
-//        private val rating1 = itemView.findViewById<ImageButton>(R.id.rating1)
-//        private val rating2 = itemView.findViewById<ImageButton>(R.id.rating2)
-//        private val rating3 = itemView.findViewById<ImageButton>(R.id.rating3)
-//        private val rating4 = itemView.findViewById<ImageButton>(R.id.rating4)
-//        private val rating5 = itemView.findViewById<ImageButton>(R.id.rating5)
-
-
-        init {
-//            ConfigUtils.getSecondaryColorOnMainBackground(itemView.context).apply {
-//                rating1.setBackgroundColor(this)
-//                rating2.setBackgroundColor(this)
-//                rating3.setBackgroundColor(this)
-//                rating4.setBackgroundColor(this)
-//                rating5.setBackgroundColor(this)
-//            }
-
-//            rating1.setOnClickListener { onRatingClickListener?.invoke(1)}
-//            rating2.setOnClickListener { onRatingClickListener?.invoke(2)}
-//            rating3.setOnClickListener { onRatingClickListener?.invoke(3)}
-//            rating4.setOnClickListener { onRatingClickListener?.invoke(4)}
-//            rating5.setOnClickListener { onRatingClickListener?.invoke(5)}
-//
-//            adapter.setOnRatingClickListener { onRatingClickListener }
-        }
-
-        override fun bindItem(item: RatingEntry) {
-            super.bindItem(item)
-//            ratingRv.apply {
-//                adapter = adapter
-//                layoutManager = LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL, false)
-//            }
-//
-//            listEntry = map(item)
-//            val listValues = item.ratingSettings?.ratingTextValues
-//            if (listValues != null) {
-//                rating1.isVisible = listValues.find { it.rating == 1 } != null
-//                ratingText1.text = listValues.find { it.rating == 1 }?.text
-//                rating2.isVisible = listValues.find { it.rating == 2 } != null
-//                ratingText2.text = listValues.find { it.rating == 2 }?.text
-//                rating3.isVisible = listValues.find { it.rating == 3 } != null
-//                ratingText3.text = listValues.find { it.rating == 3 }?.text
-//                rating4.isVisible = listValues.find { it.rating == 4 } != null
-//                ratingText4.text = listValues.find { it.rating == 4 }?.text
-//                rating5.isVisible = listValues.find { it.rating == 5 } != null
-//                ratingText5.text = listValues.find { it.rating == 5 }?.text
-//            }
-//            ratingText1.isVisible = item.ratingSettings?.type == 3
-//            ratingText2.isVisible = item.ratingSettings?.type == 3
-//            ratingText3.isVisible = item.ratingSettings?.type == 3
-//            ratingText4.isVisible = item.ratingSettings?.type == 3
-//            ratingText5.isVisible = item.ratingSettings?.type == 3
-//            if (item.ratingSettings?.size != null) {
-//                rating2.isVisible = item.ratingSettings.size == 5
-//                rating3.isVisible = item.ratingSettings.size >= 3
-//                rating4.isVisible = item.ratingSettings.size == 5
-//            }
-//            rating5.isVisible = true
-
-        }
-    }
-
-    /*private fun map(ratingEntry: RatingEntry): List<SimpleRatingEntry> {
-        val listEntry: MutableList<SimpleRatingEntry> = mutableListOf()
-        if (ratingEntry.ratingSettings?.ratingTextValues != null) {
-            for (rating in ratingEntry.ratingSettings.ratingTextValues) {
-                rating.rating?.let { listEntry.add(SimpleRatingEntry(it, rating.rating.ratingToEmojiRes(), rating.text)) }
-            }
-            return listEntry
-        }
-        if (ratingEntry.ratingSettings?.type == 1)
-            return ratingEntry.ratingSettings.size?.let { getRatingList(listEntry, it) } ?: emptyList()
-
-        return emptyList()
-    }
-
-    private fun getRatingList(
-        listEntry: MutableList<SimpleRatingEntry>,
-        size: Int,
-    ): List<SimpleRatingEntry> {
-
-        listEntry.add(SimpleRatingEntry(1, 1.ratingToEmojiRes(), null))
-        listEntry.add(SimpleRatingEntry(5, 5.ratingToEmojiRes(), null))
-        if (size == 2)
-            return listEntry
-        listEntry.add(1, SimpleRatingEntry(3, 3.ratingToEmojiRes(), null))
-        if (size == 3)
-            return listEntry
-        listEntry.add(1, SimpleRatingEntry(2, 2.ratingToEmojiRes(), null))
-        listEntry.add(3, SimpleRatingEntry(4, 4.ratingToEmojiRes(), null))
-        return listEntry
-
-    }*/
 
 
     private fun Int?.ratingToEmojiRes(): Int {
