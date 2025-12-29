0ef5b0cd73088f156affe3881145b524254bf0f9
mb <mboyarkin@pyrus.com>
2025-12-29 12:00:11 +0300

    remove prev SetPushToken command

--- PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/sdk/sync/Synchronizer.kt
+++ PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/sdk/sync/Synchronizer.kt
@@ -97,6 +97,9 @@ internal class Synchronizer(
         request: SyncRequest.Command,
     ): Try<TicketCommandResultDto> = suspendCoroutine { continuation ->
         val syncReqRes = SyncReqRes.CommandWithContinuation(request, continuation)
+        if (request is SyncRequest.Command.SetPushToken) {
+            syncLoopRequestQueue.removeIf { it.request is SyncRequest.Command.SetPushToken }
+        }
         syncLoopRequestQueue.add(syncReqRes)
         tryLoop()
     }
