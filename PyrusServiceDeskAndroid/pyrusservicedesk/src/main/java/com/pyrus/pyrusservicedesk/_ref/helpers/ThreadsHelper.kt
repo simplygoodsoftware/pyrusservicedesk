a1f07be3d74a0eecc60c0ded886472bb23d8058f
epryamonosova <epryamonosova@pyrus.com>
2026-02-06 13:46:24 +0300

    reviewed 2

--- PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/_ref/helpers/ThreadsHelper.kt
+++ PyrusServiceDeskAndroid/pyrusservicedesk/src/main/java/com/pyrus/pyrusservicedesk/_ref/helpers/ThreadsHelper.kt
@@ -6,6 +6,12 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 
 class ThreadsHelper {
+
+    /**
+     *  Performs an action in the main thread, waiting for the result.
+     *  Caution: Blocks the current thread. Do not perform complex or resource-intensive actions.
+     *  @param action - action to be performed
+     */
     fun syncRunOnMainThread(action: () -> Unit) {
         if (Looper.myLooper() == Looper.getMainLooper()) {
             action()
