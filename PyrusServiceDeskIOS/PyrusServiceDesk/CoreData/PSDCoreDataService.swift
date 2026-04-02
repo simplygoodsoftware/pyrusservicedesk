8870e90e06d9992e894b616df87e876541235ff7
sbobruskina <sbobruskina@pyrus.com>
2026-04-02 15:26:10 +0300

    2.29

--- PyrusServiceDeskIOS/PyrusServiceDesk/CoreData/PSDCoreDataService.swift
+++ PyrusServiceDeskIOS/PyrusServiceDesk/CoreData/PSDCoreDataService.swift
@@ -6,7 +6,7 @@ enum EntityType {
 }
 
 final class CoreDataService {
-    private let currentSchemaVersion = 1
+    private let currentSchemaVersion = 2
     private let schemaVersionKey = "PSDChatsSchemaVersion"
     
     lazy var persistentContainer: NSPersistentContainer = {
