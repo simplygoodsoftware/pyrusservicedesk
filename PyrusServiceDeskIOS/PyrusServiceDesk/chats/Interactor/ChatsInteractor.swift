ccfb7d60e4bde7dd0698054e15f2c45f6a4df1dc
sbobruskina <sbobruskina@pyrus.com>
2026-05-21 15:47:55 +0300

    public qr bug fix

--- PyrusServiceDeskIOS/PyrusServiceDesk/chats/Interactor/ChatsInteractor.swift
+++ PyrusServiceDeskIOS/PyrusServiceDesk/chats/Interactor/ChatsInteractor.swift
@@ -469,7 +469,7 @@ private extension ChatsInteractor {
                     }
                 } else if PyrusServiceDesk.clientId == clientId && chat.userId == (PyrusServiceDesk.customUserId ?? PyrusServiceDesk.userId) {
                     filterChats.append(chat)
-                } else if chat.userId?.count ?? 0 == 0 && chat.appId == PyrusServiceDesk.currentClientId {
+                } else if chat.userId?.count ?? 0 == 0 && PyrusServiceDesk.anonimClients.contains(PyrusServiceDesk.currentClientId ?? "") {//}&& chat.appId == PyrusServiceDesk.currentClientId {
                     filterChats.append(chat)
                 }
             }
